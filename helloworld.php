8bdc1ecd6f4842b91f93d30195561aafffa2db4b
SarzzyCIT <33451879+SarzzyCIT@users.noreply.github.com>
2017-11-14 23:10:18 +0000

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html >
-<html>
-<head>
-	<title>Hello World</title>
-</head>
-<body>
-
 <?php
-
-	$WorldVar = "World";
-	$SunVar = "Sun";
-	$MoonVar = "Moon";
-	$WorldInfo = 92897000;
-	$SunInfo = 720000000;
-	$MoonInfo = 3456;
-	
-	echo "<p>Hello $WorldVar ! <br/>";
-	echo "The $WorldVar is $WorldInfo from the $SunVar <br/>";
-	echo "Hello ", $SunVar, " <br/>";
-	echo "The $SunVar's core temp is aprox $SunInfo deg Far <br/>";
-	echo "Hello ", $MoonVar, "! <br/>";
-	echo "The $MoonVar is $MoonInfo miles diameter.</p>";
-
+	echo "This is working";
 ?>
-
-</body>
-</html>
